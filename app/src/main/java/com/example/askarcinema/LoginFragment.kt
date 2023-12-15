ff7b526fbd1d481a0acc04e8b6bc9427e55bb673
askar full belajar <108921033+askar-ef@users.noreply.github.com>
2023-12-15 23:29:02 +0700

    perbaikan login

--- app/src/main/java/com/example/askarcinema/LoginFragment.kt
+++ app/src/main/java/com/example/askarcinema/LoginFragment.kt
@@ -94,6 +94,7 @@ class LoginFragment : Fragment() {
             else -> {
                 Toast.makeText(requireContext(), "Invalid user role.", Toast.LENGTH_SHORT).show()
             }
+
         }
     }
 }
