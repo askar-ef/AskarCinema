f217100d5e9dc6fd34dcef806616727779adbe06
askar full belajar <108921033+askar-ef@users.noreply.github.com>
2023-12-18 13:44:47 +0700

    ok ok lah

--- app/src/main/java/com/example/askarcinema/MoviesFragment.kt
+++ app/src/main/java/com/example/askarcinema/MoviesFragment.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -75,10 +76,12 @@ class MoviesFragment : Fragment() {
     private fun fetchDataBasedOnConnectivity() {
         if (isNetworkAvailable()) {
             // Fetch data from Firebase and update movieList and Room Database
+            Toast.makeText(requireContext(), "Movies Updated", Toast.LENGTH_SHORT).show()
             fetchMoviesFromFirebase()
         } else {
             // Fetch data from Room Database
             fetchMoviesFromRoom()
+            Toast.makeText(requireContext(), "Connection Failed", Toast.LENGTH_SHORT).show()
         }
     }
 
